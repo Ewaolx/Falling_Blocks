cde03c919403f874df7cf9231057378a311589c1
RajSoni <rajsoni_9@yahoo.com>
2018-12-30 20:42:44 -0500

    Initial Commit

---
+++ Assets/Scripts/obstacle_spawn.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacle_spawn : MonoBehaviour {
+
+    public GameObject obstacle_prefab;
+    float screenHalfWidth;
+    float screenHalfHeight;
+    float time;
+    float timeDifficulty;
+    public float difficultyIncreaseSec  = 30;
+    public float obstacleSpeed = 3;
+    public float spawnIntervalSec = 0.8f; //Time interval between two consecutive spawns of obstacle blocks
+    public float minObstacleSize = 0.3f;
+    public float maxObstacleSize = 0.5f;
+    player playerobject;
+    // Use this for initialization
+    void Start () {
+        screenHalfWidth = Camera.main.orthographicSize * Camera.main.aspect;
+        screenHalfHeight = Camera.main.orthographicSize;
+        time = Time.time;
+        timeDifficulty = Time.time;
+        playerobject = FindObjectOfType<player>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 input = new Vector2(0,-1);
+        Vector2 velocity = input.normalized * obstacleSpeed;
+        Vector2 position = new Vector2(Random.Range(-screenHalfWidth+1,screenHalfWidth-1), screenHalfHeight - 1);
+        Vector3 rotation = new Vector3(0, 0, 0);
+
+        // Setting the angle so that it doesn't go out of screen in middle of falling
+        if(position.x>0)
+        {
+            float angle1 = Mathf.Rad2Deg * Mathf.Atan2((screenHalfWidth) + position.x , screenHalfHeight * 2 );
+            float angle2 = Mathf.Rad2Deg * Mathf.Atan2((screenHalfWidth) - position.x , screenHalfHeight * 2);
+            rotation.z = Random.Range(-angle1,angle2);
+        }
+        else if(position.x<0)
+        {
+            float angle1 = Mathf.Rad2Deg * Mathf.Atan2((screenHalfWidth) - Mathf.Abs(position.x), screenHalfHeight * 2);
+            float angle2 = Mathf.Rad2Deg * Mathf.Atan2(Mathf.Abs(position.x) + (screenHalfWidth ),screenHalfHeight * 2);
+            rotation.z = Random.Range(-angle1, angle2);
+        }
+
+
+        if(Time.time - timeDifficulty > difficultyIncreaseSec) // After every difficultyIncreaseSec seconds, the difficulty will increase by making following changes to the variables
+        {
+            print("Lapped");
+            timeDifficulty = Time.time;
+            obstacleSpeed += 1;
+            spawnIntervalSec -= 0.1f;
+            maxObstacleSize += 0.1f;
+        }
+
+        GameObject obstacle;
+        if (Time.time - time > spawnIntervalSec) //spawning obstacle every 1.5 seconds
+        {
+            //print("height" + screenHalfHeight * 2);
+            //print("width" + screenHalfWidth * 2);
+            //print("angle:" + rotation.z);
+            //print("pos in world:" + position.x);
+            obstacle = (GameObject)Instantiate(obstacle_prefab, position, Quaternion.Euler(rotation));
+            time = Time.time;
+            obstacle.transform.parent = transform;
+            obstacle.transform.localScale = new Vector3(Random.Range(minObstacleSize, maxObstacleSize), Random.Range(minObstacleSize, maxObstacleSize), 0);
+        }
+        foreach (Transform child in transform) //Iterating throught all the child's transform.
+        {
+            child.transform.Translate(velocity * Time.deltaTime,Space.Self);
+            if (child.position.y < -screenHalfHeight)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    float screenHalfWidth;
+    float speed = 5;
+	// Use this for initialization
+	void Start () {
+        //print("Screen :" + Screen.width);	
+        screenHalfWidth = Camera.main.orthographicSize * Camera.main.aspect;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        // print("Output:"+input);
+         Vector3 dir = input.normalized;
+         Vector3 vel = dir * speed;
+         transform.Translate(vel*Time.deltaTime);
+
+        Vector3 tmpPos = Camera.main.WorldToScreenPoint(transform.position);
+        if (tmpPos.x > Screen.width || tmpPos.x < 0)
+        {
+            Vector3 pos = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+            transform.position = pos;
+            print("p:");
+        }
+        /*if (transform.position.x > screenHalfWidth || transform.position.x < -screenHalfWidth)
+        {
+            Vector3 pos = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+            transform.position = pos;
+            print("p:");
+        }*/
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D triggerCollider)
+    {
+        print("Entering function");
+        if (triggerCollider.tag == "obstacleBlocks")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
